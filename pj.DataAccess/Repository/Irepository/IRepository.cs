d912e2cc8431f068a90f0ca97fa865015015602b
tankw <tantk113@gmail.com>
2023-10-05 14:29:58 +0700

    Repository pattern

---
+++ pj.DataAccess/Repository/Irepository/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pj.DataAccess.Repository.IRepository
+{
+    public interface IRepository<T> where T : class
+    {
+        //T - Category
+        IEnumerable<T> GetAll();
+        T Get1(Expression<Func<T, bool>> filter);
+        void Add(T item);
+        void Remove(T item);
+        void RemoveRange(IEnumerable<T> item);
+    }
+}
---
+++ pj.DataAccess/Repository/Repository.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore;
+using pj.DataAccess.Data;
+using pj.DataAccess.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace pj.DataAccess.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly MyAppDatabaseContext _dbContext;
+        internal DbSet<T> dbSet;
+        public Repository(MyAppDatabaseContext db)
+        {
+            _dbContext = db;
+            dbSet=_dbContext.Set<T>();
+        }
+
+
+        public void Add(T item)
+        {
+            dbSet.Add(item);    
+          //  throw new NotImplementedException();
+        }
+
+        public T Get1(Expression<Func<T, bool>> filter)
+        {
+            IQueryable<T> query = dbSet;
+            query = query.Where(filter);
+            return query.FirstOrDefault();
+            //throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            IQueryable<T> query = dbSet;
+            return query.ToList();
+            //throw new NotImplementedException();
+        }
+
+        public void Remove(T item)
+        {
+            dbSet.Remove(item);
+          //  throw new NotImplementedException();
+        }
+
+        public void RemoveRange(IEnumerable<T> item)
+        {
+            dbSet.RemoveRange(item);
+            //throw new NotImplementedException();
+        }
+    }
+}
