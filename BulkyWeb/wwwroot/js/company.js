9f6bcac9cd5cc349a4530c7ed28567c8425df219
tankw <tantk113@gmail.com>
2023-10-13 13:33:15 +0700

    company index

--- BulkyWeb/wwwroot/js/company.js
+++ BulkyWeb/wwwroot/js/company.js
@@ -8,24 +8,28 @@ function loadDataTable() {
     dataTable = $('#tblData').dataTable({
         "ajax": { url: '/admin/company/getall' },
         "columns": [
-            { data: 'id', 'width': "10%" },
-
-            { data: 'name', 'width': "15%" },
-            { data: 'stressAddress', 'width': "15%" },
-            { data: 'city', 'width': "15%" },
-            { data: 'state', 'width': "15%" },
-            { data: 'postalCode', 'width': "15%" },
-            { data: 'phoneNumber', 'width': "15%" },
-            /* {
-                 data: 'id',
-                 "render": function (data) {
-                     return `
-                     <div class="">
-                     
-                     </div>
-                     `
-                 }
-             }*/
+            { "data": 'name', 'width': "15%" },
+            { "data": 'stressAddress', 'width': "20%" },
+            { "data": 'city', 'width': "10%" },
+            { "data": 'state', 'width': "10%" },
+            { "data": 'postalCode', 'width': "10%" },
+            { "data": 'phoneNumber', 'width': "10%" },
+            {
+                data: 'id',
+                "render": function (data) {
+                    return `
+                    <div class="w-100 btn-group" role="group">
+                        <a href = "admin/company/upsert?id=${data}" class="btn btn-primary mx-2">
+                             <i class="bi bi-pencil"></i> &nbsp   Edit
+                        <a onClick=Deletef('/admin/company/delete/${data}') class=" btn btn-danger mx-2">
+                               <i class="bi bi-trash3"></i> &nbsp  Delete
+                           </a>
+                    </div>
+                    `
+                },
+                'width': "25%"
+            }
         ]
+
     });
 }
