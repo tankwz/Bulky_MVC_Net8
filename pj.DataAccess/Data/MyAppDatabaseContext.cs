44e0a3d00b678aaac18c177a9e9bbb57e5adaa6a
tankw <tantk113@gmail.com>
2023-10-14 11:35:14 +0700

    Added shopping cart to database

--- pj.DataAccess/Data/MyAppDatabaseContext.cs
+++ pj.DataAccess/Data/MyAppDatabaseContext.cs
@@ -17,6 +17,7 @@ namespace pj.DataAccess.Data
         public DbSet<Product> Products { get; set; }
         public DbSet<AppUser> AppUsers { get; set; }
         public DbSet<Company> Companies { get; set; }
+        public DbSet<ShoppingCart> shoppingCarts { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder) 
         {
             base.OnModelCreating(modelBuilder);
---
+++ pj.DataAccess/Migrations/20231014043307_add-shopping-cart-todatabase.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace pj.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class addshoppingcarttodatabase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "shoppingCarts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProductId = table.Column<int>(type: "int", nullable: false),
+                    AppUserId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    count = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_shoppingCarts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_shoppingCarts_AspNetUsers_AppUserId",
+                        column: x => x.AppUserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_shoppingCarts_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_shoppingCarts_AppUserId",
+                table: "shoppingCarts",
+                column: "AppUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_shoppingCarts_ProductId",
+                table: "shoppingCarts",
+                column: "ProductId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "shoppingCarts");
+        }
+    }
+}
