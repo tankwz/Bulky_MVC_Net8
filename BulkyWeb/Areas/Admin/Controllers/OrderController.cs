6c1fc618c976453b0bbbe5a515d5e26dac768d39
tankw <tantk113@gmail.com>
2023-10-20 00:35:01 +0700

    added filter logic

--- BulkyWeb/Areas/Admin/Controllers/OrderController.cs
+++ BulkyWeb/Areas/Admin/Controllers/OrderController.cs
@@ -6,6 +6,8 @@ using pj.DataAccess.Repository.IRepository;
 using pj.Models;
 using pj.Models.ViewModels;
 using pj.Utility;
+using System.Diagnostics;
+using System.Net.NetworkInformation;
 
 namespace BulkyWeb.Areas.Admin.Controllers
 {
@@ -15,25 +17,48 @@ namespace BulkyWeb.Areas.Admin.Controllers
     public class OrderController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
-        public OrderController(IUnitOfWork uni )
+        public OrderController(IUnitOfWork uni)
         {
             _unitOfWork = uni;
         }
         OrderVM orderVM = new();
         public IActionResult Index()
         {
-        //    if(id== null || id==0)
-        //    {
-        //        return NotFound();
-        //    }
+            //    if(id== null || id==0)
+            //    {
+            //        return NotFound();
+            //    }
             return View();
         }
 
         [HttpGet]
-        public IActionResult getAllAPI()
+        public IActionResult getAllAPI(string status)
         {
             List<OrderHead> head = _unitOfWork.OrderHead.GetAll(includeProperties: "AppUser").ToList();
-         //   if (head == null) return Json(new { data = "No Record" });
+
+            switch (status)
+            {
+                case "pending":
+                    head = head.Where(a => a.OrderStatus == SD.StatusPending).ToList();
+                    break;
+                case "paymentpending":
+                    head = head.Where(a => a.PaymentStatus == SD.PaymentStatusPending).ToList();
+                    break;
+                case "inprocess":
+                    head = head.Where(a => a.OrderStatus == SD.StatusProcessing).ToList();
+                    break;
+                case "completed":
+                    head = head.Where(a => a.OrderStatus == SD.StatusShipped).ToList();
+                    break;
+                case "approved":
+                    head = head.Where(a => a.OrderStatus == SD.StatusApproved).ToList();
+                    break;
+                default:
+
+                    break;
+            }
+
+            //   if (head == null) return Json(new { data = "No Record" });
             return Json(new { data = head });
         }
 
--- BulkyWeb/wwwroot/js/order.js
+++ BulkyWeb/wwwroot/js/order.js
@@ -1,12 +1,79 @@
 ﻿var dataTable;
-
+var url = window.location.search
 $(document).ready(function () {
-    loadDataTable();
-});
+    var url = window.location.search;
+    var urlfull = window.location.href;
 
-function loadDataTable() {
+    if (url.includes("pending")) {
+        loadDataTable("pending");
+    } else if (url.includes("paymentpending")) {
+        loadDataTable("paymentpending");
+    } else if (url.includes("inprocess")) {
+        loadDataTable("inprocess");
+    } else if (url.includes("completed")) {
+        loadDataTable("completed");
+    } else if (url.includes("approved")) {
+        loadDataTable("approved");
+    } else {
+        loadDataTable("all");
+    }
+});
+//$(document).ready(function () {
+//    var url = window.location.search
+//    console.log(url)
+//    var urlfull = window.location.href
+//    if (url.includes("pending")) {
+//        loadDataTable("pending");
+//    } else {
+//        if (url.includes("paymentpending")) {
+//            loadDataTable("paymentpending");
+//        }
+//    }
+//    else {
+//        if (url.includes("inprocess")) {
+//            loadDataTable("inprocess");
+//        }
+//    } else {
+//        if (url.includes("completed")) {
+//            loadDataTable("completed");
+//        }
+//    } else {
+//        if (url.includes("approved")) {
+//            loadDataTable("approved");
+//        }
+//    }
+//    else {
+//        loadDataTable("all");
+//    }
+//});
+
+
+/*
+    switch (status)
+    {
+        case "pending":
+            pending = style;
+            break;
+        case "paymentpending":
+            paymentpending = style;
+            break;
+        case "inprocess":
+            inprocess = style;
+            break;
+        case "completed":
+            completed = style;
+            break;
+        case "approved":
+            approved = style;
+            break;
+        default:
+            all = style;
+            break;
+    }
+*/
+function loadDataTable(status) {
     dataTable = $('#adminOrderTable').DataTable({
-        "ajax": { url: '/admin/order/getAllAPI' },
+        "ajax": { url: '/admin/order/getAllAPI?status=' + status },
         "columns": [
             { data: 'id', "width": "5%" },
             { data: 'name', "width": "15%" },
@@ -31,3 +98,53 @@ function loadDataTable() {
 
 
 }
+
+
+
+
+/*
+var dataTable;
+
+$(document).ready(function () {
+var url = window.location.search
+var urlfull = window.location.href
+if (url.includes("pending"))
+loadDataTable("pending");
+else if (url.includes("paymentpending")
+loadDataTable("paymentpending");
+else if (url.includes("inprocess")
+loadDataTable("inprocess");
+else if (url.includes("completed")
+loadDataTable("completed");
+else if (url.includes("approved")
+loadDataTable("approved");
+else loadDataTable("all");
+
+});
+function loadDataTable(status) {
+dataTable = $('#adminOrderTable').DataTable({
+"ajax": { url: '/admin/order/getAllAPI?status=' + status },
+"columns": [
+{ data: 'id', "width": "5%" },
+{ data: 'name', "width": "15%" },
+{ data: 'appUser.email', "width": "15%" },
+{ data: 'phoneNumber', "width": "15%" },
+{ data: 'orderStatus', "width": "10%" },
+{ data: 'paymentStatus', "width": "10%" },
+{ data: 'orderTotal', "width": "10%" },
+{
+    data: 'id',
+    "render": function (data) {
+        return ` 
+            <div class="w-75 ">
+                <a href="/admin/order/details?orderId=${data}" class="btn btn-primary mx-2"><i class="bi bi-layout-text-window-reverse"></i> Details</a>
+            </div> 
+        `
+    },
+    "width": "20%"
+}
+]
+})
+
+
+}*/
